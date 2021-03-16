be6487f4ecaf608bc6f41918e3b8d69a7fa13e1c
Daniel <snakefangox@gmail.com>
2021-03-16 18:30:26 +1300

    Remove remappedSource

--- remappedSrc/net/snakefangox/worldshell/collision/ShellLocationChecker.java
+++
@@ -1,9 +0,0 @@
-package net.snakefangox.worldshell.collision;
-
-/**
- * Vanilla box checks only go 2 blocks over chunk borders.
- * We need much more than that, hence this.
- */
-public class ShellLocationChecker {
-
-}
